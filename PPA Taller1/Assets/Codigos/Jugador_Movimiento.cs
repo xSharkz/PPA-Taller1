b45b96244e5cb6ec30553203fd581dad88e74d0c
xSharkz <martin.becerrarissi@gmail.com>
2024-03-19 23:19:23 -0300

    style(animaciones del perro y objetos):
    -Se añadieron las animaciones fundamentales del perro.
    -Se añadió la animación de la moneda y el objeto dentro del juego.

--- PPA Taller1/Assets/Codigos/Jugador_Movimiento.cs
+++ PPA Taller1/Assets/Codigos/Jugador_Movimiento.cs
@@ -10,32 +10,56 @@ public class Jugador_Movimiento : MonoBehaviour
 
 
     Rigidbody2D rb2D;
+    SpriteRenderer sR;
+    Animator animator;
 
     private bool mirandoDerecha = true;
 
     private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-
+        sR = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
+        //Esta cayendo
+        if (!esEnSuelo.esSuelo)
+        {
+            animator.SetBool("esCayendo", true);
+        }
+        //Esta en suelo
+        else
+        {
+            animator.SetBool("esCayendo", false);
+        }
+        //Esta corriendo hacia la derecha
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2D.velocity = new Vector2(velocidadMovimiento, rb2D.velocity.y);
+            sR.flipX = false;
+            animator.SetBool("esCorriendo", true);
         }
+        //Esta corriendo hacia la izquierda
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2D.velocity = new Vector2(-velocidadMovimiento, rb2D.velocity.y);
+            sR.flipX = true;
+            animator.SetBool("esCorriendo", true);
         }
+        //Esta quieto
         else
         {
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            animator.SetBool("esCorriendo", false);
         }
+        //Esta saltando
         if(Input.GetKey("space") && esEnSuelo.esSuelo)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.y, velocidadSalto);
+            animator.SetBool("esCorriendo", true);
         }
+    
     }
 }
